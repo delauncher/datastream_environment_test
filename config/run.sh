8312d5e6136b195efbf867eeb74a381f82fee8d8
datastreamenv <famersbs@gmail.com>
2015-09-29 06:04:29 -0700

    modify zookeeper images

--- config/run.sh
+++ config/run.sh
@@ -1,4 +1,4 @@
 cd /opt/delauncher/delauncher
 node app.js instance $INSTANCE_ID $CONFIG_FILE_PATH
 
-# /opt/zookeeper/bin/zkServer.sh start-foreground
+ /opt/zookeeper/bin/zkServer.sh start-foreground
